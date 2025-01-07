460355e817f8a0495091744427deaabf34360173
Riyan <riyanhunxai55@gmail.com>
2025-01-07 16:44:19 +0500

    changes

---
+++ home_task/task2.js
@@ -0,0 +1,29 @@
+class Family {
+  constructor(name, lname) {
+    this.name = name;
+    this.lname = lname;
+  }
+
+  introduction() {
+    return `My name is ${this.name} and last name is ${this.lname}`;
+  }
+}
+
+class ExtendedFamily extends Family {
+  constructor(name, lname, age) {
+    super(name, lname);//parent class ka constru ko pass krny ka lya
+    this.age = age;
+  }
+
+  getDetail() {
+    return `${this.introduction()} - Age: ${this.age}`;
+  }
+}
+
+let family1 = new Family("Saima", "Akhtar");  //details pass kiya ha
+let family2 = new ExtendedFamily("Riyan", "Hunxai", 20);
+let family3 = new ExtendedFamily("hamza", "Khan", 25);
+
+console.log(family1.introduction());
+console.log(family2.getDetail());
+console.log(family3.getDetail()); 
